8739d121336339702d7ae72a6a69d9fbaabced8a
Alvaro-Sargento <sargentoa2@gmail.com>
2024-04-22 00:54:42 +0200

    exempls de controller e rotas

---
+++ app/Http/Controllers/Site/siteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+class siteController {
+    public function contact(){
+        return view('site/contact');
+    }
+}
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Site\siteController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/contact', [siteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
